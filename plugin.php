38d2c11d060587dcc36f4e59b040acaf62421406
Scott Duncombe <srduncombe@gmail.com>
2012-03-22 17:54:54 -0700

    Initial Commit

---
+++ plugin.php
@@ -0,0 +1,16 @@
+<?php 
+/*
+Plugin Name: Better Google Forms
+Plugin URI: https://github.com/mojowen/Better-Google-Forms
+Description: Embeds Google forms in posts, but better!
+Author: Scott Duncombe
+Version: 0.5
+Author URI: http://scottduncombe.com/
+*/
+
+// Setting a base path. Easy change if the code is going to be incorporated into a theme, use get_bloginfo('theme_directory') instead
+$base = WP_PLUGIN_URL . '/' . str_replace(basename( __FILE__), "" ,plugin_basename(__FILE__));
+define('better_google_forms_base', $base);
+
+
+?>
